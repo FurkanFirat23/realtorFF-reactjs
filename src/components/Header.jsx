62bb1c8d8db6ca3454bd64a54a9311d764546dc1
FURKAN FIRAT <furkan.0096@gmail.com>
2022-12-01 23:15:07 +0300

    created the header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+export default function Header() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  function pathMathRoute(route) {
+    if (route === location.pathname) {
+      return true;
+    }
+  }
+  return (
+    <div className="bg-white border-b shadow-sm sticky top-0 z-10">
+      <header className="flex justify-between items-center px-4 max-w-6xl mx-auto">
+        <div>
+          <img
+            src="https://fontmeme.com/temporary/644bc635cf61dfaab73eacb6579fe010.png"
+            alt="logo"
+            className="h-5 cursor-pointer"
+            onClick={() => navigate("/")}
+          />
+        </div>
+        <div>
+          <ul className="flex space-x-12">
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/")}
+            >
+              Home
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/offers") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/offers")}
+            >
+              Offers
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/sign-in") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/sign-in")}
+            >
+              Sign in
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
