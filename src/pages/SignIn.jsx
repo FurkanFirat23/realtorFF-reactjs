7de784dc3a676596f14b5164ac8439b544e0fcbf
FURKAN FIRAT <furkan.0096@gmail.com>
2022-12-02 22:31:45 +0300

    signin css + some changes

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,24 @@
 import React from "react";
 
 export default function SignIn() {
-  return <div>Sign In</div>;
+  return (
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
+
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto ">
+        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+          <img
+            src="https://plus.unsplash.com/premium_photo-1661423767020-5f190fa792e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80"
+            alt="key"
+            className="w-full rounded-2xl "
+          />
+        </div>
+        <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+          <form>
+            <input className="w-full" type="text" />
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
