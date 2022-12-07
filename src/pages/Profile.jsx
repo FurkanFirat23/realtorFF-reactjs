b9ef97bfc9b549d01fa1eb57005a08c8688ab640
FURKAN FIRAT <furkan.0096@gmail.com>
2022-12-08 00:25:16 +0300

    edit button completed

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
 import { useState } from "react";
+import { doc, updateDoc } from "firebase/firestore";
 import { useNavigate } from "react-router";
-
+import { toast } from "react-toastify";
+import { db } from "../firebase";
 export default function Profile() {
   const auth = getAuth();
   const navigate = useNavigate();
@@ -17,6 +19,32 @@ export default function Profile() {
     auth.signOut();
     navigate("/");
   }
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
+  async function onSubmit() {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        // update display name in firebase auth
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+        // update name in the firestore
+
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef, {
+          name,
+        });
+      }
+      toast.success("Profile updated!");
+    } catch (error) {
+      toast.error("Could not update profile detail");
+    }
+  }
+
   return (
     <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
       <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
@@ -27,8 +55,11 @@ export default function Profile() {
             type="name"
             id="name"
             value={name}
-            disabled
-            className="w-full px-4 py-2 text-2xl text-gray-600 bg-white border border-gray-300 rounded-xl transition ease-in-out mb-6"
+            disabled={!editDetail}
+            onChange={onChange}
+            className={`w-full px-4 py-2 text-2xl text-gray-600 bg-white border border-gray-300 rounded-xl transition ease-in-out mb-6 ${
+              editDetail && "bg-red-300 focus:bg-red-300"
+            }`}
           />
           {/* email */}
           <input
@@ -42,7 +73,10 @@ export default function Profile() {
             <p className="flex items-center mb-6">
               Do you want to change your name ?
               <span
-                onClick={setEditDetail((prevState) => !prevState)}
+                onClick={() => {
+                  editDetail && onSubmit();
+                  setEditDetail((prevState) => !prevState);
+                }}
                 className="text-red-600 hover:text-red-800 transition ease-in-out duration-200 ml-2 cursor-pointer"
               >
                 {editDetail ? "Apply change" : "Edit"}
