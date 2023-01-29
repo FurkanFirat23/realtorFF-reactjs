c7a04286482975443d5d2b7de535e944d0f0dc50
FURKAN FIRAT <furkan.0096@gmail.com>
2023-01-30 02:34:44 +0300

    firebase and env files configs

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,14 +10,22 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
-
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDxqw412ZqvMKF6dWOAlq9uZL2mVwnNPac",
+//   authDomain: "realtorff-a4ffa.firebaseapp.com",
+//   projectId: "realtorff-a4ffa",
+//   storageBucket: "realtorff-a4ffa.appspot.com",
+//   messagingSenderId: "313005019057",
+//   appId: "1:313005019057:web:cba4f76b23a8bb9169f790",
+//   measurementId: "G-G4BFSR7JLY",
+// };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const db = getFirestore();
+
+export const db = getFirestore(app);
