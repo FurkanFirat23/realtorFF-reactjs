e9e897272282244f4c25b37fad9b6211dce699c1
Furkan Fırat <furkan.0096@gmail.com>
2023-05-10 15:26:29 +0300

    oAuth comments added

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -7,28 +7,33 @@ import { toast } from "react-toastify";
 import { db } from "../firebase";
 import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 import { useNavigate } from "react-router";
+
 export default function OAuth() {
   const navigate = useNavigate();
+
   async function onGoogleClick() {
     try {
-      const auth = getAuth();
-      const provider = new GoogleAuthProvider();
-      const result = await signInWithPopup(auth, provider);
-      const user = result.user;
-      // check user
-      const docRef = doc(db, "users", user.uid);
-      const docSnap = await getDoc(docRef);
+      const auth = getAuth(); // Retrieve the Firebase authentication object.
+      const provider = new GoogleAuthProvider(); // Create a Google authentication provider.
+      const result = await signInWithPopup(auth, provider); // Perform the sign-in process with a Google popup.
+      const user = result.user; // Get the information of the signed-in user.
+
+      // Check the user
+      const docRef = doc(db, "users", user.uid); // Create a document reference in the Firestore collection to store the user's credentials.
+      const docSnap = await getDoc(docRef); // Get the document and check if it exists.
 
       if (!docSnap.exists()) {
+        // If the document does not exist, create a new one.
         await setDoc(docRef, {
           name: user.displayName,
           email: user.email,
           timestamp: serverTimestamp(),
         });
       }
-      navigate("/");
+
+      navigate("/"); // Redirect the user to the homepage.
     } catch (error) {
-      toast.error("Could not authorize with Google");
+      toast.error("Could not authorize with Google"); // Show an error message if an error occurs.
     }
   }
 
